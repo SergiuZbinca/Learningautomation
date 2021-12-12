1f33788b00fd6ba0039e5474c5e241b6cf940153
SergiuZbinca <sergiugabriell@yahoo.com>
2021-12-12 10:59:50 +0200

    Added Homework_1
    Exercise_1_Display  password in console
    Exercise_2_Create calculator

---
+++ homework_1.py
@@ -0,0 +1,44 @@
+# exercitiul_1_Afisare in consola parola este:*****
+
+# expected_usr = 'Admin'
+# expected_pwd = 'abcd1234'
+# sold = 10000
+#
+# import getpass
+#
+# user_name = input('Introduceti utilizatorul: ')
+# assert user_name == expected_usr
+# pwd = getpass(expected_pwd)
+# assert pwd == expected_pwd
+# input('Apasati enter pentru logare')
+# print('Soldul dvs este: ' + str(sold))
+# cash_out = int(input('Introduceti suma pentru retragere: '))
+# print('Soldul ramas este: ', sold - cash_out)
+
+# ************************************************************************
+
+# exercitiul_2_calculator
+
+# a = float(input('Enter a value for a: '))
+# b = float(input('Enter a value for b: '))
+
+# def add(a,b):
+#     return a + b
+# def substract(a,b):
+#     return a - b
+# def multiply(a,b):
+#     return a * b
+# def divide(a,b):
+#     return a / b
+#
+# choice = input('Enter choice: \n 1 for add \n 2 for substract \n 3 for multiply \n 4 for divide \n ')
+# if choice == '1':
+#     print(f'The value of a+b is: {add(a,b)}')
+# elif choice == '2':
+#     print(f'The value of a-b is: {substract(a,b)}')
+# elif choice == '3':
+#     print(f'The value of a*b is: {multiply(a,b)}')
+# elif choice == '4':
+#     print(f'The value of a/b is: {divide(a,b)}')
+
+# ****************************************************************************************************************
