acc3402b54121def88619ee55fdf059f3e1b6bf9
SergiuZbinca <sergiugabriell@yahoo.com>
2021-12-29 13:48:41 +0200

    homework_1

--- src/part_1_coding/lesson_1.py
+++ src/part_1_coding/lesson_1.py
@@ -1,23 +1,21 @@
-# """
-# introduction
-# """
-#
-# expected_usr = 'Admin'
-# expected_pwd = 'abcd1234'
-# sold = 10000
-#
-# user_name = input('Introduceti utilizatorul: ')
-# assert user_name == expected_usr
-# pwd = input('Introduceti parola: ')
-# assert pwd == expected_pwd
-# input('Apasati enter pentru logare')
-# print('Soldul dvs este: ' + str(sold))
-# cash_out = int(input('Introduceti suma pentru retragere: '))
-# print('Soldul ramas este: ', sold - cash_out)
+"""
+ introduction
+"""
 
-# print(f'Your user name is: {user_name}, and your password is: {pwd}','Log in with succes', sep='*', end='\n')
-# print(f'Your user name is: {user_name}, and your password is: {pwd}','Log in with succes', sep='*', end='---')
+expected_usr = 'Admin'
+expected_pwd = 'abcd1234'
+sold = 10000
+
+user_name = input('Introduceti utilizatorul: ')
+assert user_name == expected_usr
+pwd = input('Introduceti parola: ')
+assert pwd == expected_pwd
+input('Apasati enter pentru logare')
+print('Soldul dvs este: ' + str(sold))
+cash_out = int(input('Introduceti suma pentru retragere: '))
+print('Soldul ramas este: ', sold - cash_out)
+
+print(f'Your user name is: {user_name}, and your password is: {pwd}','Log in with succes', sep='*', end='\n')
+print(f'Your user name is: {user_name}, and your password is: {pwd}','Log in with succes', sep='*', end='---')
 
 # ******************************************************
-
-
